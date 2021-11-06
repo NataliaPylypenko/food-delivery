36b8d07dc21ea6bf59f49fde18262f5258ddfc0a
n.pilipenko <n.pilipenko@smileexpo.com.ua>
2021-11-06 12:11:08 +0200

    modal

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,4 @@
 
 import './tabs'
 import './timer'
+import './modal'
---
+++ src/js/modal.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    function openModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+        // clearInterval(modalTimerId);
+    }
+
+    function closeModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    // close modal
+    // 1) by clicking on the cross
+    modalCloseBtn.addEventListener('click', closeModal);
+    // 2) by clicking outside modal
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+    // 3) by clicking Escape
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    // open modal
+    // 1) by clicking on any button Contact us
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    // 2) after a while
+    // const modalTimerId = setTimeout(openModal, 3000);
+
+    // 3) when scrolling
+    // if the user has scrolled to the end
+    function showModalByScroll() {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll);
+
+})
